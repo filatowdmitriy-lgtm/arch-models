6c501bcb709498ba9a19b9f89c3a98eeb5cdf817
filatowdmitriy-lgtm <filatow.dmitriy@gmail.com>
2025-12-23 04:30:40 +0300

    Update threePreview.js

--- preview/threePreview.js
+++ preview/threePreview.js
@@ -111,7 +111,7 @@ function updateCameraPosition(camera) {
   const r = state.radius;
 
   // предметная дистанция (НЕ математическая)
-  const dist = r * 1.0;
+  const dist = r * 0.85;
 
   camera.position.set(
     -dist * 0.7,   // слева
